672e76068ac0c19c25ead0af5ccf5651f07f84de
Carrick Hong <swgrhck@gmail.com>
2019-08-11 01:41:17 +0800

    Configure the java source to 1.8 and encoding to UTF-8

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,3 +13,12 @@ dependencies {
     compileOnly("org.apiguardian:apiguardian-api:1.1.0")
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
 }
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
